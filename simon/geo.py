03f261fcaf023086773cbfdcf4d3506f460baf49
Andy Dirnberger <dirn@dirnonline.com>
2013-01-01 13:51:07 -0500

    Move geospatial helpers to their own module
    The original intention of the `query` module was to house the `QuerySet` class. Over time it grew to contain `Q` and the geospatial helper methods as well. While I'm opting to leaving `Q` in the `query` module I decided it was more appropriate for the geospatial methods to be placed in their own module.
    
    By adding a `geo` module, all methods associated with geospatial queries can be found and viewed without being mixed in with the unrelated `Q` and `QuerySet` classes.

---
+++ simon/geo.py
@@ -0,0 +1,207 @@
+"""Helper methods to ease geospatial queries."""
+
+__all__ = ('box', 'circle', 'near', 'polygon', 'within')
+
+import collections
+
+
+def _validate_point(point, name=None, alternate_type=None):
+    """Validates the type and length of a point.
+
+    This method defines a point as either a ``list`` of exactly two
+    elements. If will also accept a ``tuple``.
+
+    :param point: The point to validate.
+    :type point: list.
+    :param name: (optional) A descriptive name to use when an
+                 exception is raised.
+    :type name: str.
+    :param alternate_type: (optional) Alternate type(s) to check for.
+    :type alternate_type: type or tuple of types.
+    :raises: :class:`TypeError`, :class:`ValueError`.
+
+    .. versionadded:: 0.1.0
+    """
+
+    exception = None
+
+    type_to_check = alternate_type or (list, tuple)
+
+    if not isinstance(point, type_to_check):
+        exception = TypeError
+    elif len(point) != 2:
+        exception = ValueError
+
+    if exception is not None:
+        message = '{0} must be a list containing exactly 2 elements'
+        raise exception(message.format(name or '`point`'))
+
+
+def box(lower_left_point, upper_right_point):
+    """Builds a ``$box`` query.
+
+    This is a convenience method for ``$within`` queries that use
+    ``$box`` as their shape.
+
+    ``lower_left_point`` and ``upper_right_point`` are a pair of
+    coordinates, each as a ``list``, that combine to define the bounds
+    of the box in which to search.
+
+    :param lower_left_point: The lower-left bound of the box.
+    :type lower_left_point: list.
+    :param upper_right_point: The upper-right bound of the box.
+    :type upper_right_point: list.
+    :returns: dict -- the ``$box`` query.
+    :raises: :class:`TypeError`, :class:`ValueError`.
+
+    .. versionadded:: 0.1.0
+    """
+
+    _validate_point(lower_left_point, '`lower_left_point`')
+    _validate_point(upper_right_point, '`upper_right_point`')
+
+    return within('box', lower_left_point, upper_right_point)
+
+
+def circle(point, radius):
+    """Builds a ``$circle`` query.
+
+    This is a convenience method for ``$within`` queries that use
+    ``$circle`` as their shape.
+
+    :param point: The center of the circle.
+    :type point: list.
+    :param radius: The distance from the center of the circle.
+    :type radius: int.
+    :returns: dict -- the ``$circle`` query.
+    :raises: :class:`TypeError`, :class:`ValueError`.
+
+    .. versionadded:: 0.1.0
+    """
+
+    _validate_point(point)
+
+    return within('circle', point, radius)
+
+
+def near(point, max_distance=None, unique_docs=False):
+    """Builds a ``$near`` query.
+
+    This is a convenience method for more complex ``$near`` queries. For
+    simple queries that simply use the point, the regular query syntax
+    of ``field__near=[x, y]`` will suffice. This method provides a way
+    to include ``$maxDistance`` and (if support is added)
+    ``$uniqueDocs`` without needing to structure the query as
+    ``field={'$near': [x, y], '$maxDistance': z}``.
+
+    **Note** 2012-11-29 As of the current release of MongoDB (2.2),
+    ``$near`` queries do not support the ``$uniqueDocs`` parameter. It
+    is included here so that when support is added to MongoDB, no
+    changes to the library will be needed.
+
+    :param point: The point to use for the geospatial lookup.
+    :type point: list, containing exactly two elements.
+    :param max_distance: (optional) The maximum distance a point can be
+                         from ``point``.
+    :type max_distance: int.
+    :param unique_docs: (optional) If ``True`` will only return unique
+                        documents.
+    :returns: dict -- the ``$near`` query.
+    :raises: :class:`TypeError`, :class:`ValueError`.
+
+    .. versionadded:: 0.1.0
+    """
+
+    _validate_point(point)
+
+    # All queries containing the $near point
+    query = {'$near': point}
+
+    # Check for and add any of the optional operators as necessary
+    if max_distance is not None:
+        query['$maxDistance'] = max_distance
+    if unique_docs:
+        query['$uniqueDocs'] = unique_docs
+
+    return query
+
+
+def polygon(*points):
+    """Builds a ``$polygon`` query.
+
+    This is a convenience method for ``$within`` queries that use
+    ``$polygon`` as their shape.
+
+    ``points`` should either be expressed as a series of ``list``'s or a
+    single ``dict`` containing ``dict``'s providing pairs of coordinates
+    that behind the polygon.
+
+    :param \*points: The bounds of the polygon.
+    :type \*points: \*args.
+    :returns: dict -- the ``$polygon`` query.
+    :raises: :class:`TypeError`, :class:`ValueError`.
+
+    .. versionadded:: 0.1.0
+    """
+
+    if len(points) > 1:
+        # If there are more than one point, they should be a series of
+        # lists defining the coordinates. They should be passed into
+        # within() as *args.
+        for p in points:
+            _validate_point(p, 'Each point')
+
+        return within('polygon', *points)
+
+    elif points:
+        # If there is one point, it should be a dictionary of nested
+        # dictionaries providing the coordinates. They should be passed
+        # into within() as **kwargs.
+        points = points[0]
+
+        if not isinstance(points, collections.Mapping):
+            raise TypeError('`points` must either be a list of points or a '
+                            'dict mapping of points.')
+        if len(points) < 2:
+            raise ValueError('`points` must either be a list of points or a '
+                             'dict mapping of points.')
+
+        for k, p in points.iteritems():
+            _validate_point(p, name='Each point',
+                            alternate_type=collections.Mapping)
+
+        return within('polygon', **points)
+
+    # At this point, there are no points
+    raise TypeError('`points` must either be a list of points or a '
+                    'dict mapping of points.')
+
+
+def within(shape, *bounds, **bounds_map):
+    """Builds a ``$within`` query.
+
+    This is a convenience method for ``$within`` queries.
+
+    :param shape: The shape of the bounding area.
+    :type shape: str.
+    :param \*bounds: Coordinate pairs defining the bounding area.
+    :type \*bounds: \*args.
+    :param \*\*bounds_map: Named coordinate pairs defining the bounding area.
+    :type \*\*bounds_map: \*\*kwargs.
+    :returns: dict -- the ``$within`` query.
+    :raises: :class:`RuntimeError`.
+
+    .. versionadded:: 0.1.0
+    """
+
+    if bounds and bounds_map:
+        raise RuntimeError("Only one of 'bounds' and 'bounds_map' can be "
+                           "provided.")
+
+    # **kwargs trumps *args here. This decision was made so the *args
+    # can be forced into a list with ease.
+    bounds = bounds_map or list(bounds)
+
+    query = {'$within': {'${0}'.format(shape): bounds}}
+
+    return query
---
+++ tests/test_geo.py
@@ -0,0 +1,278 @@
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+import collections
+import mock
+
+from simon import geo
+
+
+class TestGeo(unittest.TestCase):
+    """Test the helper methods in the `query` module."""
+
+    def test_box(self):
+        """Test the `box()` method."""
+
+        with mock.patch('simon.geo.within') as within:
+            geo.box([1, 2], [3, 4])
+            within.assert_called_with('box', [1, 2], [3, 4])
+
+        expected = {'$within': {'$box': [[1, 2], [3, 4]]}}
+        actual = geo.box([1, 2], [3, 4])
+        self.assertEqual(actual, expected)
+
+    def test_box_typeerror(self):
+        """Test that `box()` raises `TypeError`."""
+
+        with self.assertRaises(TypeError):
+            geo.box(1, [2, 3])
+
+        with self.assertRaises(TypeError):
+            geo.box([1, 2], 3)
+
+    def test_box_valueerror(self):
+        """Test that `box()` raises `ValueError`."""
+
+        with self.assertRaises(ValueError):
+            geo.box([1, 2, 3], [4, 5])
+
+        with self.assertRaises(ValueError):
+            geo.box([1, 2], [3, 4, 5])
+
+    def test_circle(self):
+        """Test the `circle()` method."""
+
+        with mock.patch('simon.geo.within') as within:
+            geo.circle([1, 2], 3)
+            within.assert_called_with('circle', [1, 2], 3)
+
+        expected = {'$within': {'$circle': [[1, 2], 3]}}
+        actual = geo.circle([1, 2], 3)
+        self.assertEqual(actual, expected)
+
+    def test_circle_typeerror(self):
+        """Test that `circle()` raises `TypeError`."""
+
+        with self.assertRaises(TypeError):
+            geo.circle(1, 2)
+
+    def test_circle_valueerror(self):
+        """Test that `circle()` raises `ValueError`."""
+
+        with self.assertRaises(ValueError):
+            geo.circle([1, 2, 3], 4)
+
+    def test_near(self):
+        """Test the `near()` method."""
+
+        expected = {'$near': [1, 2]}
+        actual = geo.near([1, 2])
+        self.assertEqual(actual, expected)
+
+        expected = {'$near': [1, 2], '$maxDistance': 3}
+        actual = geo.near([1, 2], max_distance=3)
+        self.assertEqual(actual, expected)
+
+        expected = {'$near': [1, 2], '$uniqueDocs': True}
+        actual = geo.near([1, 2], unique_docs=True)
+        self.assertEqual(actual, expected)
+
+        expected = {'$near': [1, 2], '$maxDistance': 3, '$uniqueDocs': True}
+        actual = geo.near([1, 2], max_distance=3, unique_docs=True)
+        self.assertEqual(actual, expected)
+
+        # near() supports point as a tuple
+        expected = {'$near': (1, 2)}
+        actual = geo.near((1, 2))
+        self.assertEqual(actual, expected)
+
+    def test_near_typeerror(self):
+        """Test that `near()` raises `TypeError`."""
+
+        with self.assertRaises(TypeError):
+            geo.near(1)
+
+    def test_near_valueerror(self):
+        """Test that `near()` raises `ValueError`."""
+
+        with self.assertRaises(ValueError):
+            geo.near([1])
+
+        with self.assertRaises(ValueError):
+            geo.near([1, 2, 3])
+
+    def test_polygon(self):
+        """Test the `polygon()` method."""
+
+        with mock.patch('simon.geo.within') as within:
+            geo.polygon([1, 2], [3, 4], [5, 6])
+            within.assert_called_with('polygon', [1, 2], [3, 4], [5, 6])
+
+            geo.polygon({'a': {'x': 1, 'y': 2}, 'b': {'x': 3, 'y': 4}})
+            within.assert_called_with('polygon', a={'x': 1, 'y': 2},
+                                      b={'x': 3, 'y': 4})
+
+        expected = {'$within': {'$polygon': [[1, 2], [3, 4], [5, 6]]}}
+        actual = geo.polygon([1, 2], [3, 4], [5, 6])
+        self.assertEqual(actual, expected)
+
+        expected = {'$within': {'$polygon': {'a': {'x': 1, 'y': 2},
+                                             'b': {'x': 3, 'y': 4}}}}
+        actual = geo.polygon({'a': {'x': 1, 'y': 2}, 'b': {'x': 3, 'y': 4}})
+        self.assertEqual(actual, expected)
+
+    def test_polygon_mappings_typeerror(self):
+        """Test that `polygon()` raises `TypeError` with mappings."""
+
+        with self.assertRaises(TypeError):
+            geo.polygon({'a': {'b': 1, 'c': 2}, 'd': 3})
+
+    def test_polygon_mappings_valueerror(self):
+        """Test that `polygon()` raises `ValueError` with mappings."""
+
+        with self.assertRaises(ValueError):
+            geo.polygon({'a': 1})
+
+        with self.assertRaises(ValueError):
+            geo.polygon({'a': {'b': 1}})
+
+        with self.assertRaises(ValueError):
+            geo.polygon({'a': {'b': 1, 'c': 2}})
+
+        with self.assertRaises(ValueError):
+            geo.polygon({'a': {'b': 1, 'c': 2}, 'd': {'e': 3}})
+
+    def test_polygon_typeerror(self):
+        """Test that `polygon()` raises `TypeError`."""
+
+        with self.assertRaises(TypeError):
+            geo.polygon()
+
+        with self.assertRaises(TypeError):
+            geo.polygon(1)
+
+        with self.assertRaises(TypeError):
+            geo.polygon([1])
+
+        with self.assertRaises(TypeError):
+            geo.polygon([1, 2, 3])
+
+        with self.assertRaises(TypeError):
+            geo.polygon([1, 2], 3)
+
+    def test_polygon_valueerror(self):
+        """Test that `polygon()` raises `ValueError`."""
+
+        with self.assertRaises(ValueError):
+            geo.polygon([1, 2], [3])
+
+        with self.assertRaises(ValueError):
+            geo.polygon([1, 2], [3, 4, 5])
+
+    def test_validate_point_alternate_type(self):
+        """Test the `_validate_point()` method with alternate types."""
+
+        geo._validate_point({'a': 1, 'b': 2},
+                              alternate_type=collections.Mapping)
+
+    def test_validate_point_alternate_type_typeerror(self):
+        ("Test that `_validate_point()` raises `TypeError` with "
+         "alternate types.")
+
+        with self.assertRaises(TypeError):
+            geo._validate_point(1, alternate_type=basestring)
+
+        with self.assertRaises(TypeError):
+            geo._validate_point('a', alternate_type=int)
+
+    def test_validate_point_alternate_type_valueerror(self):
+        ("Test that `_validate_point()` raises `ValueError` with "
+         "alternate types.")
+
+        with self.assertRaises(ValueError):
+            geo._validate_point({'a': 1},
+                                  alternate_type=collections.Mapping)
+
+        with self.assertRaises(ValueError):
+            geo._validate_point({'a': 1, 'b': 2, 'c': 3},
+                                  alternate_type=collections.Mapping)
+
+    def test_validate_point_name(self):
+        """Test the `_validate_point()` method with names."""
+
+        with self.assertRaises(TypeError) as e:
+            geo._validate_point(1, 'name')
+
+        expected = 'name must be a list containing exactly 2 elements'
+        actual = e.exception.message
+        self.assertEqual(actual, expected)
+
+        with self.assertRaises(ValueError) as e:
+            geo._validate_point([1], 'name')
+
+        expected = 'name must be a list containing exactly 2 elements'
+        actual = e.exception.message
+        self.assertEqual(actual, expected)
+
+    def test_validate_point_typeerror(self):
+        """Test that `_validate_point()` raises `TypeError`."""
+
+        # Test with a string
+        with self.assertRaises(TypeError) as e:
+            geo._validate_point('a')
+
+        expected = '`point` must be a list containing exactly 2 elements'
+        actual = e.exception.message
+        self.assertEqual(actual, expected)
+
+        # Test with an int
+        with self.assertRaises(TypeError) as e:
+            geo._validate_point(1)
+
+        expected = '`point` must be a list containing exactly 2 elements'
+        actual = e.exception.message
+        self.assertEqual(actual, expected)
+
+    def test_validate_point_valueerror(self):
+        """Test the `_validate_point()` method."""
+
+        # Test with 1 element
+        with self.assertRaises(ValueError) as e:
+            geo._validate_point([1])
+
+        expected = '`point` must be a list containing exactly 2 elements'
+        actual = e.exception.message
+        self.assertEqual(actual, expected)
+
+        # Test with 3 elements
+        with self.assertRaises(ValueError) as e:
+            geo._validate_point([1, 2, 3])
+
+        expected = '`point` must be a list containing exactly 2 elements'
+        actual = e.exception.message
+        self.assertEqual(actual, expected)
+
+        # The following should not raise an exception
+        geo._validate_point([1, 2])
+        geo._validate_point((1, 2))
+
+    def test_within(self):
+        """Test the `within()` method."""
+
+        expected = {'$within': {'$box': [[1, 2], [3, 4]]}}
+        actual = geo.within('box', [1, 2], [3, 4])
+        self.assertEqual(actual, expected)
+
+        expected = {'$within': {'$polygon': {'a': {'x': 1, 'y': 2},
+                                             'b': {'x': 3, 'y': 4}}}}
+        actual = geo.within('polygon', a={'x': 1, 'y': 2},
+                              b={'x': 3, 'y': 4})
+        self.assertEqual(actual, expected)
+
+    def test_within_runtimeerror(self):
+        """Test that `within()` raises `RuntimeError`."""
+
+        with self.assertRaises(RuntimeError):
+            geo.within('shape', *[1, 2], **{'a': {'x': 3, 'y': 4}})
