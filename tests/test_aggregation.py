a05ee88d3179f9b5a31a55a6ee164429ff390506
Andy Dirnberger <dirn@dirnonline.com>
2013-04-06 21:36:48 -0400

    Clean up
    The `Pipeline` class was originally going to be named `Aggregate` so I
    named all of the test instances `a` (just like `Model` test instances
    are usually named `m`). Because the class name actually starts with a
    'P', I'm renaming `a` to `p`.

--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -23,112 +23,112 @@ class TestPipeline(unittest.TestCase):
     def test___init__(self):
         """Test the `__init__()` method."""
 
-        a = aggregation.Pipeline()
+        p = aggregation.Pipeline()
 
-        self.assertEqual(a._project, {})
-        self.assertEqual(a._match, {})
-        self.assertIsNone(a._limit)
-        self.assertIsNone(a._skip)
-        self.assertEqual(a._unwind, {})
-        self.assertEqual(a._group, {})
-        self.assertEqual(a._geonear, {})
+        self.assertEqual(p._project, {})
+        self.assertEqual(p._match, {})
+        self.assertIsNone(p._limit)
+        self.assertIsNone(p._skip)
+        self.assertEqual(p._unwind, {})
+        self.assertEqual(p._group, {})
+        self.assertEqual(p._geonear, {})
 
-        self.assertIsNone(a._cls)
+        self.assertIsNone(p._cls)
 
     def test__init__class(self):
         """Test the `__init__()` method with a class."""
 
-        a = aggregation.Pipeline(cls=Model)
+        p = aggregation.Pipeline(cls=Model)
 
-        self.assertTrue(a._cls is Model)
+        self.assertTrue(p._cls is Model)
 
     def test_project_add_or_remove(self):
         """Test the `add_or_remove()` function."""
 
-        a = aggregation.Pipeline()
-        a.project(include='a')
+        p = aggregation.Pipeline()
+        p.project(include='a')
 
-        self.assertEqual(a._project, {'a': 1})
+        self.assertEqual(p._project, {'a': 1})
 
     def test_project_add_or_remove_list(self):
         """Test the `add_or_remove()` function with a list."""
 
-        a = aggregation.Pipeline()
-        a.project(include=['a'])
+        p = aggregation.Pipeline()
+        p.project(include=['a'])
 
-        self.assertEqual(a._project, {'a': 1})
+        self.assertEqual(p._project, {'a': 1})
 
     def test_project_add_or_remove_no_mapped_fields(self):
         ("Test the `add_or_remove()` function with an empty field "
          "map.")
 
-        a = aggregation.Pipeline(cls=NotMappedModel)
+        p = aggregation.Pipeline(cls=NotMappedModel)
 
         with mock.patch('simon.aggregation.get_nested_key') as get_nested_key:
-            a.project(include=['a'])
+            p.project(include=['a'])
 
             get_nested_key.assert_not_called()
 
     def test_consecutive_calls(self):
         """Test that `project()` properly handles consecutive calls."""
 
-        a = aggregation.Pipeline()
-        a.project(include='a')
-        a.project(include='b')
-        a.project(exclude=('a', 'c'))
+        p = aggregation.Pipeline()
+        p.project(include='a')
+        p.project(include='b')
+        p.project(exclude=('a', 'c'))
 
-        self.assertEqual(a._project, {'a': 0, 'b': 1, 'c': 0})
+        self.assertEqual(p._project, {'a': 0, 'b': 1, 'c': 0})
 
     def test_project_exclude(self):
         """Test the `project()` method with excludes."""
 
-        a = aggregation.Pipeline()
-        a.project(exclude=('a', 'b'))
+        p = aggregation.Pipeline()
+        p.project(exclude=('a', 'b'))
 
-        self.assertEqual(a._project, {'a': 0, 'b': 0})
+        self.assertEqual(p._project, {'a': 0, 'b': 0})
 
     def test_project_exclude_mapped_field(self):
         """Test the `project()` method with an excluded mapped field."""
 
-        a = aggregation.Pipeline(cls=MappedModel)
-        a.project(exclude='fake')
-        self.assertEqual(a._project, {'real': 0})
+        p = aggregation.Pipeline(cls=MappedModel)
+        p.project(exclude='fake')
+        self.assertEqual(p._project, {'real': 0})
 
-        a = aggregation.Pipeline(cls=MapNestedModel)
-        a.project(exclude=('a.b', 'd'))
-        self.assertEqual(a._project, {'c': 0, 'e.f': 0})
+        p = aggregation.Pipeline(cls=MapNestedModel)
+        p.project(exclude=('a.b', 'd'))
+        self.assertEqual(p._project, {'c': 0, 'e.f': 0})
 
     def test_project_include(self):
         """Test the `project()` method with includes."""
 
-        a = aggregation.Pipeline()
-        a.project(include=('a', 'b'))
+        p = aggregation.Pipeline()
+        p.project(include=('a', 'b'))
 
-        self.assertEqual(a._project, {'a': 1, 'b': 1})
+        self.assertEqual(p._project, {'a': 1, 'b': 1})
 
     def test_project_include_exclude(self):
         """Test the `project()` method with includes and excludes."""
 
-        a = aggregation.Pipeline()
-        a.project(include=('a', 'b'), exclude=('c', 'd'))
+        p = aggregation.Pipeline()
+        p.project(include=('a', 'b'), exclude=('c', 'd'))
 
-        self.assertEqual(a._project, {'a': 1, 'b': 1, 'c': 0, 'd': 0})
+        self.assertEqual(p._project, {'a': 1, 'b': 1, 'c': 0, 'd': 0})
 
     def test_project_include_mapped_field(self):
         """Test the `project()` method with an included mapped field."""
 
-        a = aggregation.Pipeline(cls=MappedModel)
-        a.project(include='fake')
-        self.assertEqual(a._project, {'real': '$fake'})
+        p = aggregation.Pipeline(cls=MappedModel)
+        p.project(include='fake')
+        self.assertEqual(p._project, {'real': '$fake'})
 
-        a = aggregation.Pipeline(cls=MapNestedModel)
-        a.project(include=('a.b', 'd'))
-        self.assertEqual(a._project, {'c': '$a.b', 'e.f': '$d'})
+        p = aggregation.Pipeline(cls=MapNestedModel)
+        p.project(include=('a.b', 'd'))
+        self.assertEqual(p._project, {'c': '$a.b', 'e.f': '$d'})
 
     def test_project_return(self):
         """Test that `project()` returns the instance."""
 
-        a = aggregation.Pipeline()
-        b = a.project(include='a')
+        p1 = aggregation.Pipeline()
+        p2 = p1.project(include='a')
 
-        self.assertEqual(a, b)
+        self.assertEqual(p1, p2)
